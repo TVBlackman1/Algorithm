e9aca676e00dc25dda14773db9d241f50a93457a
TVBlackman1 <41297903+TVBlackman1@users.noreply.github.com>
2019-02-13 18:05:04 +0300

    find bags

--- graph/Floyd_Warshall_algorithm.cpp
+++ graph/Floyd_Warshall_algorithm.cpp
@@ -42,17 +42,6 @@ int main()
             }
         }
     }
-
-    for(int k = 0; k < n; k++)
-    {
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                matrix[i][j] = _min(matrix[i][j], matrix[i][k] + matrix[k][j]);
-            }
-        }
-    }
     // work this array
 
     for(int i = 0; i < n; i++)
