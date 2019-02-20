996daf893b9ef10ecbc1d6c74b4ccc665aa1e824
TVBlackman1 <41297903+TVBlackman1@users.noreply.github.com>
2019-02-20 22:02:13 +0300

    Update Dijkstra's_algorithm.py
    add path

--- graph/Dijkstra's_algorithm/realise_1/Dijkstra's_algorithm.py
+++ graph/Dijkstra's_algorithm/realise_1/Dijkstra's_algorithm.py
@@ -16,6 +16,8 @@ not_used = [True for _ in rng]  # visited points
 dist = [inf for i in rng]  # path to points
 dist[S] = 0
 
+prev = [-1] * n  # for path
+
 for i in rng:
     index_to_use = -1
 
@@ -26,10 +28,23 @@ for i in rng:
 
     for j in rng:
         if j != index_to_use and matrix[index_to_use][j] != -1:
+            if dist[index_to_use] + matrix[index_to_use][j] < dist[j]:
+                 dist[j] = dist[index_to_use] + matrix[index_to_use][j]
+                 
             dist[j] = min(dist[j], dist[index_to_use] + matrix[index_to_use][j])
+            prev[j] = index_to_use  # for path
     #  find min
 
     not_used[index_to_use] = False
 
 for num, i in enumerate(dist):
     print(num, i, sep=" => ")
+    
+#  for path:
+way = [N]
+x = prev[N-1]
+ 
+while x != -1:
+    way.append(x+1)
+    x = prev[x]
+print(*reversed(way))
